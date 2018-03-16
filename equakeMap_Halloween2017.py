36b74bbde468a35a91b5500a1094bb5330397427
benfernandez <fernandezb@gwu.edu>
2018-03-16 11:34:57 -0400

    added pumpkin logo for ~spooky~ effect

--- equakeMap_Halloween2017.py
+++ equakeMap_Halloween2017.py
@@ -1,5 +1,8 @@
 import folium
 import pandas
+from folium.plugins import FloatImage
+
+
 
 df= pandas.read_csv("equake_10_31_2017.csv")
 
@@ -7,8 +10,11 @@ lat=list(df["latitude"])
 lon=list(df["longitude"])
 magnitude=list(df["mag"])
 
+pumpImage = ('pumpkinIcon.png')
+
 map = folium.Map(location=[38.889931,-77.009003],zoom_start=5,tiles="stamentoner")
 
+FloatImage(pumpImage,bottom=5,left=90).add_to(map)
 
 fgquake = folium.FeatureGroup(name="Earthquakes 10_31_2017")
 
