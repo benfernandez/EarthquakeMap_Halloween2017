890d5c7316979d9c41843624d1238ae399521116
benfernandez <fernandezb@gwu.edu>
2018-03-16 10:50:56 -0400

    created initial map with quakes plotted

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # EarthquakeMap_Halloween2017
 A webmap showing all the earthquakes on 10-31-2017 with data pulled from earthquake.usgs.gov
+
+Sample image of map:
+
+![alt-text][map]
+
+[map]: screenshot_map.png
--- equakeMap_Halloween2017.py
+++ equakeMap_Halloween2017.py
@@ -5,3 +5,16 @@ df= pandas.read_csv("equake_10_31_2017.csv")
 
 lat=list(df["latitude"])
 lon=list(df["longitude"])
+magnitude=list(df["mag"])
+
+map = folium.Map(location=[38.889931,-77.009003],zoom_start=5,tiles="stamentoner")
+
+
+fgquake = folium.FeatureGroup(name="Earthquakes 10_31_2017")
+
+for la,ln,ma in zip(lat,lon,magnitude):
+    fgquake.add_child(folium.Marker(location=[la,ln],popup=folium.Popup("magnitude is: " +str(ma)),icon=folium.Icon(color='orange')))
+
+map.add_child(fgquake)
+
+map.save("EQMap_Hween2017.html")
