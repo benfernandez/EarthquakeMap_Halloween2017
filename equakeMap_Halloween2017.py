8839d3880f9637938a2fa91baff11adbe2945ed7
benfernandez <fernandezb@gwu.edu>
2018-03-21 10:58:21 -0400

    added legend and updated image

--- equakeMap_Halloween2017.py
+++ equakeMap_Halloween2017.py
@@ -13,13 +13,23 @@ magnitude=list(df["mag"])
 pumpImage = ('pumpkinIcon.png')
 
 def magColorChanger(magnitude):
-    if magnitude < 1.0:
+    if magnitude <= 1.0:
         return 'green'
-    elif 1.0 < magnitude < 4.0:
+    elif 1.0 < magnitude <= 4.0:
         return 'orange'
     elif magnitude > 4.0:
         return 'red'
 
+legend =   '''
+                <div style="position: fixed;
+                            bottom: 50px; left: 50px; width: 220px; height: 75px;
+                            border:2px solid grey; z-index:9999; font-size:16px;
+                            ">&nbsp; Green: mag < 1.0  <br>
+                              &nbsp; Orange: 1.0 < mag < 4.0 &nbsp;<br>
+                              &nbsp; Red: mag > 4.0 &nbsp;<br>
+
+                </div>
+                '''
 map = folium.Map(location=[38.889931,-77.009003],zoom_start=5,tiles="stamenterrain")
 
 FloatImage(pumpImage,bottom=5,left=90).add_to(map)
@@ -32,4 +42,5 @@ for la,ln,ma in zip(lat,lon,magnitude):
 
 
 map.add_child(fgquake)
+map.get_root().html.add_child(folium.Element(legend))
 map.save("EQMap_Hween2017.html")
