8c6611b899524f2f5848935c63819175abd4ed70
benfernandez <fernandezb@gwu.edu>
2018-03-21 10:35:20 -0400

    changed maptiles, marker type, and added colorchange based off magnitude

--- equakeMap_Halloween2017.py
+++ equakeMap_Halloween2017.py
@@ -12,15 +12,24 @@ magnitude=list(df["mag"])
 
 pumpImage = ('pumpkinIcon.png')
 
-map = folium.Map(location=[38.889931,-77.009003],zoom_start=5,tiles="stamentoner")
+def magColorChanger(magnitude):
+    if magnitude < 1.0:
+        return 'green'
+    elif 1.0 < magnitude < 3.0:
+        return 'orange'
+    elif magnitude > 3.0:
+        return 'red'
+
+map = folium.Map(location=[38.889931,-77.009003],zoom_start=5,tiles="stamenterrain")
 
 FloatImage(pumpImage,bottom=5,left=90).add_to(map)
 
 fgquake = folium.FeatureGroup(name="Earthquakes 10_31_2017")
 
 for la,ln,ma in zip(lat,lon,magnitude):
-    fgquake.add_child(folium.Marker(location=[la,ln],popup=folium.Popup("magnitude is: " +str(ma)),icon=folium.Icon(color='orange')))
+    fgquake.add_child(folium.CircleMarker(location=[la,ln],radius = 8,popup="magnitude is: " +str(ma),fill_color=magColorChanger(ma),
+    color='grey',fill_opacity=.6,fill=True))
+
 
 map.add_child(fgquake)
-
 map.save("EQMap_Hween2017.html")
